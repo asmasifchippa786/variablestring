e81644a1ddc48550da0b541a3ac5a2731aa04915
asmaasifchippa786 <asmaasifchippa786@gmail.com>
2022-05-26 13:47:25 +0500

    1st

---
+++ app.js
@@ -0,0 +1,55 @@
+var username;
+
+var myName = "M . Anas Asif Chippa";
+
+var message = "Hello World";
+
+alert(message);
+
+var firstName = "Ali";
+
+var lastName = "Khan";
+
+alert("My name is " + firstName + " " + lastName);
+
+var year = "12 Years old";
+
+alert("I am " + year);
+
+var study = "Saylani";
+
+alert("I study in " + study);
+
+var learn = "Web & Mobile Application-Batch(8)";
+
+alert("I am the student of " + learn);
+
+var food = "PIZZA";
+
+alert(food + "\n" + food.substring(0,4) + "\n" + food.substring(0,3) + "\n" + food.substring(0,2) + "\n" + food.substring(0,1));
+
+var work = "Software Developer";
+
+var loc = "Karachi,Pakistan";
+
+var wifeName = "Sara";
+
+var kidsNumber = "2 Kids";
+
+alert("Your name is " + myName + ". And you wil be a " + work + ". You live in " + loc + ". and married with " + wifeName + ". With " + kidsNumber);
+
+var emil = "abc@xyz.com";
+
+alert("My email Id is " + emil);
+
+var book = "A smarter way to learn Javascript";
+
+alert("I am trying to learn from the book '" + book + "'");
+
+console.log("My name is " + myName);
+
+console.log("I am study in " + study);
+
+console.log("I am the student of " + learn);
+
+console.log("I am " + year);
